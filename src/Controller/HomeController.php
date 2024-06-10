e863fdbb6f7217b3b0e0cd1ea35895304e82babb
Louis Arnault <louisarnault@gmail.com>
2024-06-10 16:52:51 +0200

    Add form request handling

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,9 @@ use App\Form\AddPlayerType;
 use App\Form\AddTeamType;
 use App\Repository\PlayerRepository;
 use App\Repository\TeamRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -33,15 +35,33 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/add/team', name: 'app_add_team', methods: ['POST'])]
-    public function addTeam(): Response
+    #[Route('/team/add', name: 'app_add_team', methods: ['POST'])]
+    public function addTeam(Request $request, EntityManagerInterface $manager): Response
     {
+        $form = $this->createForm(AddTeamType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $team = $form->getData();
+            $manager->persist($team);
+            $manager->flush();
+        }
+
         return $this->redirectToRoute('app_home');
     }
 
-    #[Route('/add/player', name: 'app_add_player', methods: ['POST'])]
-    public function addPlayer(): Response
+    #[Route('/player/add', name: 'app_add_player', methods: ['POST'])]
+    public function addPlayer(Request $request, EntityManagerInterface $manager): Response
     {
+        $form = $this->createForm(AddPlayerType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $player = $form->getData();
+            $manager->persist($player);
+            $manager->flush();
+        }
+
         return $this->redirectToRoute('app_home');
     }
 }
