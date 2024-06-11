da674e9eaa715f8e14f8eb9e8f9f363783f88b8a
Louis Arnault <louisarnault@gmail.com>
2024-06-11 10:51:31 +0200

    Fix flash message

--- importmap.php
+++ importmap.php
@@ -28,10 +28,6 @@ return [
     'tom-select' => [
         'version' => '2.3.1',
     ],
-    'tom-select/dist/css/tom-select.default.css' => [
-        'version' => '2.3.1',
-        'type' => 'css',
-    ],
     'tom-select/dist/css/tom-select.bootstrap5.css' => [
         'version' => '2.3.1',
         'type' => 'css',
